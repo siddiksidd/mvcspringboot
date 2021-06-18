124eee318451b9dc2ce78b6c0e08f3ce8cedbbc3
S I D D I K <S I D D I K@DESKTOP-PAUOTNE>
2021-06-18 13:01:05 +0530

    mvcspringboot

---
+++ src/test/java/com/vm/MvcspringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcspringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
